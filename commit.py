e03007923c0a52bd8c770ca715c2cb04173e5a2c
chriscarm <110430152+chriscarm@users.noreply.github.com>
2023-10-13 23:50:28 +0000

    Auto commit

--- commit.py
+++
@@ -1,1 +0,0 @@
-import os; os.system('git add . && git commit -m "Resolve pylint issues" && git push origin main')
--- openai_key.py
+++
@@ -1,9 +0,0 @@
-"""
-Module to set OpenAI API key from environment variable.
-"""
-import os
-import openai
-
-
-# Set OpenAI API Key from environment variable
-openai.api_key = os.getenv("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")
