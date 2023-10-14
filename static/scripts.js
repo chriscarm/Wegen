eb24a2b58ac503b6b2f44554b79d013aad8b1be2
chriscarm <110430152+chriscarm@users.noreply.github.com>
2023-10-14 20:20:13 +0000

    working

--- static/scripts.js
+++ static/scripts.js
@@ -1,7 +1,4 @@
 document.getElementById("chat-bar").addEventListener("click", function() {
-    // JavaScript to handle chat bar and chat overlay behavior
-
-document.getElementById("chat-bar").addEventListener("click", function() {
     const chatOverlay = document.getElementById("chat-overlay");
     chatOverlay.style.display = "block";
     chatOverlay.style.opacity = 0;
@@ -18,4 +15,25 @@ document.getElementById("close-chat").addEventListener("click", function() {
     }, 300); // Delay to match the fade-out duration
 });
 
-});
+async function ask() {
+    const question = document.getElementById("question").value;
+    const responseElement = document.getElementById("response");
+    
+    // Clear the previous response
+    responseElement.innerText = "Thinking...";
+    
+    try {
+        const response = await fetch("/ask", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: `question=${encodeURIComponent(question)}`,
+        });
+        const data = await response.json();
+        responseElement.innerText = data.answer;
+    } catch (error) {
+        console.error("Error:", error);
+        responseElement.innerText = "Sorry, I couldn't understand that. Please try again.";
+    }
+}
